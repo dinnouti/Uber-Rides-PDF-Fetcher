213205bf4c258c36255e44a0353647f47d2847e4
Alex Dinnouti <dinnouti@gmail.com>
2025-05-02 21:22:50 +0000

    docs: updating readme

--- uber-receipt-pdf.user.js
+++ uber-receipt-pdf.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Uber Rides Fetcher
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Fetch Uber ride history and download receipts
 // @author       You
 // @match        https://riders.uber.com/*
